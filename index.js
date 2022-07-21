c60b901c984b6ff3e032cc1a74e10c72df7fe77d
Marcus Vinícius Richa <marcus.richa@protonmail.com>
2022-07-21 19:37:36 -0300

    Create showBookList() and start to assemble each html node from list

--- index.js
+++ index.js
@@ -4,11 +4,12 @@
 const uiNodeMsg = document.getElementById('form-validate');
 const thirdWallOrnament = document.getElementById('third-ornament');
 
-const addBookBtn = document.getElementById('add-book');
+const booklistNode = document.getElementById('booklist');
 const nodeTitle = document.getElementById('title');
 const nodeAuthor = document.getElementById('author');
 let nodePages = 0;
 let nodeState = false;
+const addBookBtn = document.getElementById('add-book');
 
 let bookState = 'not read yet';
 
@@ -48,7 +49,70 @@ const theTwoTowers = new Book('The Two Towers', 'J.R.R. Tolkien', 395, 'not read
 const theReturnOfTheKing = new Book('The Return of the King', 'J.R.R. Tolkien', 395, 'not read yet');
 
 myLibrary.push(theHobbit, theFellowship, theTwoTowers, theReturnOfTheKing);
-console.log(myLibrary);
+//console.log(myLibrary);
+
+showBooklist(myLibrary);
+
+
+
+
+/*
+<li class="book" >
+	<img class="div-color" src="./public/images/icons8-book-64.png" alt="books icon" />
+	<div>
+		<h3>The Hobbit</h3>
+		<h5>J.R.R. Tolkien</h5>
+		<p>295 pages</p>
+		<p>
+			not read yet 
+			<span>
+				<input 
+					type="checkbox" 
+					id="is-read" 
+					name="is-read" 
+				/>
+			</span>
+		</p>
+	</div>
+</li>
+*/
+function showBooklist(list) {
+	let newNode = list.map((book) => {
+		const li = createDOMNode('li', '', 'book');
+		const img = createDOMNode('img', '', 'div-color');
+		const div = createDOMNode('div');
+		const h3 = createDOMNode('h3', book.title, '');
+		const h5 = createDOMNode('h5', book.author, '');
+		const para1 = createDOMNode('p', book.numPages, '');
+		const para2 = createDOMNode('p', book.bookState, '');
+		const span = createDOMNode('span');
+		const input = createDOMNode('input');
+		
+		li.appendChild(img);
+		
+		div.appendChild(h3);
+		div.appendChild(h5);
+		div.appendChild(para1);
+		
+		span.appendChild(input);
+		para2.appendChild(span);
+		div.appendChild(para2);
+		
+		li.appendChild(div);
+		
+		return li;
+	});
+	
+	
+	
+	
+	console.log(newNode);
+	
+	//booklistNode
+}
+
+
+
 
 
 
@@ -62,7 +126,7 @@ function addBookToLibrary(title, author, numPages, booksRead) {
 		}
 		
 		if (!booksRead) {
-			booksRead = 'not read yet';
+			booksRead = bookState; //'not read yet';
 		}
 		
 		const book = new Book(title, author, numPages, booksRead);
@@ -77,12 +141,19 @@ function addBookToLibrary(title, author, numPages, booksRead) {
 	}  
 }
 
-
-function createDOMNode(node = 'p', msg) {
+/* AUXILIARY FUNCTIONS */
+function createDOMNode(node = 'p', msg, cssClass) {
 	const newNode = document.createElement(node);
-	const textNode = document.createTextNode(msg);
 	
-	newNode.appendChild(textNode);
+	if (msg) {
+		const textNode = document.createTextNode(msg);
+		
+		newNode.appendChild(textNode);
+	}
+	
+	if (cssClass) {
+		newNode.classList.add(cssClass);
+	}
 	
 	return newNode;
 }
