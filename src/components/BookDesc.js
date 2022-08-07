2d73aaa7649fcbb674c7c07d96157b1a629f6f1b
Marcus Vinícius Richa <marcus.richa@protonmail.com>
2022-08-07 13:38:18 -0300

    Change Books methods name. Create BookDesc model and html element

---
+++ src/components/BookDesc.js
@@ -0,0 +1,17 @@
+import { createDOMNode } from './auxUIFunctions.js';
+
+
+// UI nodes
+const booklistPartition = document.querySelector('.booklist-partition');
+
+function showBookDesc() {
+	
+	//let newUINode = createDOMNode(
+	node, message, cssClass);
+	
+	
+}
+
+
+export default showBookDesc;
+
--- src/model/Book.js
+++ src/model/Book.js
@@ -18,10 +18,10 @@ function Book(title, author, numPages, bookState) {
 	this.author = author;
 	this.coauthors = [];
 	this.numVolumes = 1;
-	this.publisher = '';
+	this.publisher = 'Fantastic Bookshelf';
 	this.bookType = 'hard corver';
 	this.bookCategory = 'fiction';
-	this.bookDesc = '';
+	this.bookDesc = 'This is the greatest book of all time!!!';
 	this.edition = 1;
 	this.releaseYear = new Date();
 	this.numPages = numPages;
@@ -30,11 +30,16 @@ function Book(title, author, numPages, bookState) {
 	this.lastChange = new Date();
 } 
 
-Book.prototype.info = function() {
-	const info = `${this.title} by ${this.author}, ${this.numPages} pages, ${this.bookState}`;
+Book.prototype.shortDesc = function() {
+	const shortDesc = `${this.title} by ${this.author}, ${this.numPages} pages, ${this.bookState}`;
 			
-	return info;
+	return shortDesc;
 }
 
+Book.prototype.bigDesc = function() {
+	const bigDesc = `${this.title} by ${this.author} has ${this.numVolumes} volumes. It's about ${this.bookCategory} and was published in the format ${this.bookType} by ${this.publisher}, in the year ${this.releaseYear}, ${this.bookState}. ${this.bookDesc}`;
+	
+	return bigDesc;
+}
 
 export default Book;
